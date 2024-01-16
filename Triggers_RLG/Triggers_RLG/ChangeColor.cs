1f4ab59bfbea9a36ceb2566c386cfe250232b953
rigolook <rixxormen@gmail.com>
2024-01-16 09:11:22 -0700

    first commit

---
+++ Triggers_RLG/Triggers_RLG/ChangeColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Triggers_RLG
+{
+    public class ChangeColor : TriggerAction<Button>
+    {
+        protected override async void  Invoke(Button btn)
+        {
+            await Task.Delay(500);
+            btn.BackgroundColor = Color.DarkOrange;
+            btn.Text = "Aqui vamos!!!";
+            await Task.Delay(500);
+            btn.BackgroundColor = Color.MediumPurple;
+            btn.Text = "DALEEEE!!!";
+        }
+    }
+
+}
---
+++ Triggers_RLG/Triggers_RLG/SliderColorTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Triggers_RLG
+{
+    public class SliderColorTrigger : TriggerAction<Slider>
+    {
+        protected override void Invoke(Slider sender)
+        {
+            var value = sender.Value;
+
+            if (value < 40)
+                sender.BackgroundColor = Color.Red;
+            else if (value > 60)
+                sender.BackgroundColor = Color.Green;
+            else
+                sender.BackgroundColor = Color.Aqua;
+        }
+
+    }
+}
